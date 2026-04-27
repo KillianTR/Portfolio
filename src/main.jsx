9200184c168cbbb2bfd58d5fb6dac7930a03ba7f
Killian Torrell <killiantorrell@gmail.com>
2026-04-27 14:29:22 +0200

    feat: add vercel analytics and speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./styles.css";
+// 1. Importamos los componentes de Vercel
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      {/* 2. Los colocamos aquí abajo */}
+      <Analytics />
+      <SpeedInsights />
     </BrowserRouter>
   </React.StrictMode>
 );
